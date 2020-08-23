bba0b18cadd9ffb7f09e604585f1a8f026628cb5
Bunlong <bunlong.van@gmail.com>
2020-08-23 21:00:13 +0700

    :rocket: v3.7.3

--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/docs":["static\u002Fchunks\u002Fpages\u002Fdocs.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
