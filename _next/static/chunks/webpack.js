00da9bc7ce69bfb47bfd011528b362740e7a6b1d
Bunlong <bunlong.van@gmail.com>
2021-05-01 15:37:30 +0700

    :arrow_up: Improve SEO

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4cd8dc6fb38c5a3e4402";
+/******/ 	var hotCurrentHash = "6319bdfd6f4ff2eff422";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
